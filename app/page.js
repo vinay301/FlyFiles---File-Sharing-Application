0e1ded16511f02990bd7bc86d448c8e40896cd30
vinay <vinaysharma.vs743@gmail.com>
2024-01-28 18:53:55 +0530

    Initial Setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "./_components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      Home
+    </div>
+  );
+}
